bd6f9bddd6a345a596680d1d10a779597a510ed4
HarryHeres <personal@janheres.eu>
2024-04-20 00:49:48 +0200

    Added Tmux, updated Neovim

--- update.sh
+++ update.sh
@@ -12,7 +12,9 @@ mkdir ./$HOME_FOLDER
 cp ~/.zprofile ./$HOME_FOLDER
 cp ~/.zshrc ./$HOME_FOLDER
 cp ~/.clangd ./$HOME_FOLDER
+cp ~/.tmux.conf ./$HOME_FOLDER
 
+cp -r ~/.config/alacritty ./$CONFIG_FOLDER
 cp -r ~/.config/kitty ./$CONFIG_FOLDER
 cp -r ~/.config/nvim ./$CONFIG_FOLDER
 cp -r ~/.config/sioyek ./$CONFIG_FOLDER
