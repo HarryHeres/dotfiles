774dc7b33fab453723324ef9e491228c46044996
HarryHeres <personal@janheres.eu>
2024-04-20 01:09:32 +0200

    Updated Neovim config

--- dotconfig/nvim/lua/harry/lazy_plugins/harpoon.lua
+++ dotconfig/nvim/lua/harry/lazy_plugins/harpoon.lua
@@ -1,19 +1,19 @@
 return {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" },
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
 
-    config = function()
-        local harpoon = require('harpoon')
+	config = function()
+		local harpoon = require('harpoon')
 
-        harpoon:setup()
+		harpoon:setup()
 
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-        vim.keymap.set("n", "<leader>e", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+		vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+		vim.keymap.set("n", "<leader>e", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-        vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
-    end
+		vim.keymap.set("n", "h1", function() harpoon:list():select(1) end)
+		vim.keymap.set("n", "h2", function() harpoon:list():select(2) end)
+		vim.keymap.set("n", "h3", function() harpoon:list():select(3) end)
+		vim.keymap.set("n", "h4", function() harpoon:list():select(4) end)
+	end
 }
