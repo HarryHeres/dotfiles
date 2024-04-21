b424f1616e52d6aa40b2b6bea6e6c3fbaa3aa85d
HarryHeres <personal@janheres.eu>
2024-04-21 17:41:10 +0200

    Updated Neovim LSP config for completion

--- dotconfig/nvim/lua/harry/lazy_plugins/treesitter.lua
+++ dotconfig/nvim/lua/harry/lazy_plugins/treesitter.lua
@@ -1,32 +1,32 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    config = function()
-        require 'nvim-treesitter.configs'.setup({
-            -- A list of parser names, or "all" (the five listed parsers should always be installed)
-            ensure_installed = { "cpp", "java", "c", "lua", "vim", "vimdoc", "sql", "latex", "markdown", "html" },
+	'nvim-treesitter/nvim-treesitter',
+	config = function()
+		require 'nvim-treesitter.configs'.setup({
+			-- A list of parser names, or "all" (the five listed parsers should always be installed)
+			ensure_installed = { "cpp", "java", "c", "lua", "vim", "vimdoc", "sql", "latex", "markdown", "html", "comment" },
 
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
+			-- Install parsers synchronously (only applied to `ensure_installed`)
+			sync_install = false,
 
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-            auto_install = false,
+			-- Automatically install missing parsers when entering buffer
+			-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+			auto_install = false,
 
-            -- indent = {
-            --   enable = true
-            -- },
+			-- indent = {
+			--   enable = true
+			-- },
 
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-                disable = { "csv" },
-            },
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+				disable = { "csv" },
+			},
 
-            autotag = {
-                enable = true
-            }
-        })
+			autotag = {
+				enable = true
+			}
+		})
 
-        vim.treesitter.language.register('c', 'lisp') -- the someft filetype will use the python parser and queries.
-    end
+		vim.treesitter.language.register('c', 'lisp') -- the someft filetype will use the python parser and queries.
+	end
 }
